3dc7bd2f4f5ae1add1ea910308038546c25dbeef
Dmayank297 <139100813+Dmayank297@users.noreply.github.com>
2024-12-29 02:58:24 +0530

    Added navigation

---
+++ app/src/main/java/com/example/mathapp/NavigationRoutes.kt
@@ -0,0 +1,18 @@
+package com.example.mathapp
+
+const val LogInScreen = "LogInScreen"
+const val SignUpScreen = "SignUpScreen"
+const val SplashScreen = "SplashScreen"
+const val HomeScreen = "HomeScreen"
+const val HomeContentScreen = "HomeContent"
+
+const val MessageId = "messageId"
+const val MessageDefaultId = "-1"
+const val MessageIdArg = "?$MessageId={$MessageId}"
+const val DescriptionId = "descriptionId"
+const val DescriptionDefaultId = MessageDefaultId
+const val DescriptionIdArg = "?$DescriptionId={$DescriptionId}"
+
+const val LocalHost = "10.0.2.2"
+const val AuthPort = 9099
+const val FireStorePort = 8080
